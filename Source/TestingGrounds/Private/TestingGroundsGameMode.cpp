dbe57c1fac2ea716b7184d62aa43b6554323acc7
RazielSun <razielsun@gmail.com>
2019-02-13 17:13:28 +0300

    fixes c classes, update animations

--- Source/TestingGrounds/Private/TestingGroundsGameMode.cpp
+++ Source/TestingGrounds/Private/TestingGroundsGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "TestingGroundsGameMode.h"
 #include "TestingGroundsHUD.h"
-#include "TestingGroundsCharacter.h"
+#include "FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ATestingGroundsGameMode::ATestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprints/BP_FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
